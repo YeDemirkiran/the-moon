5101d01a7923e2b925b49617193922637acbce84
YeDemirkiran <yedemirkiran27@gmail.com>
2023-12-02 22:49:53 +0300

    Add sun and rotating earth

---
+++ Assets/EarthRotation.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EarthRotation : MonoBehaviour
+{
+    [SerializeField] Axis axis;
+    [SerializeField] float degreesPerSecond, pivotMultiplier;
+    [SerializeField] Transform centerPivot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 delta = new Vector3(0f, 0f, 0f);
+
+        switch (axis)
+        {
+            case Axis.x:
+                delta.x = degreesPerSecond;
+                break;
+            case Axis.y:
+                delta.y = degreesPerSecond;
+                break;
+            case Axis.z:
+                delta.z = degreesPerSecond;
+                break;
+        }
+
+        transform.eulerAngles += delta * Time.deltaTime;
+
+        transform.RotateAround(centerPivot.position, Vector3.up, degreesPerSecond * pivotMultiplier * Time.deltaTime);
+    }
+}
